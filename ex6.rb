d1914028b1c01902a229840d2bd810c15d4c6393
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 15:55:52 -0800

    this completes the exercises withe output including variable insertion

--- ex6.rb
+++ ex6.rb
@@ -1,13 +1,13 @@
 # defines the number of types of people as '10'
 types_of_people = 10
 # prints a string including the number of types of people as a variable
-x = 'There are #{types_of_people} types of people.'
+x = "There are #{types_of_people} types of people."
 # defines the variable binary
-binary = 'binary'
+binary = "binary"
 # defines the variable do_not as a contraction
-do_not = 'don\'t'
+do_not = "don't"
 # prints a string including variables binary and do_not
-y = 'Those who know #{binary} and those who #{do_not}.'
+y = "Those who know #{binary} and those who #{do_not}."
 
 # prints the string x
 puts x
@@ -15,22 +15,22 @@ puts x
 puts y
 
 # prints a line including the string x as a variable
-puts 'I said: #{x}.'
+puts "I said: #{x}."
 # prints a line including the string y as a variable
-puts 'I also said \'#{y}\'.'
+puts "I also said '#{y}'."
 
 # defines the variable hilarious as the string 'false'
-hilarious = 'false'
+hilarious = "false"
 # defines the variable joke_evaluation as a string containing the variable false
-joke_evaluation = 'Isn\'t that joke so funny?! #{hilarious}'
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
 # prints variable joke_evaluation
 puts joke_evaluation
 
 # defines variable w as a string
-w = 'This is the left side of...'
+w = "This is the left side of..."
 # defines the variable e as a string
-e = 'a string with a right side.'
+e = "a string with a right side."
 
 # prints the variables w and e together to output a single string
 puts w + e
