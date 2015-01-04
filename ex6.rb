5142ccd85452da210142ae8db37764e0cc12168e
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 15:52:08 -0800

    complete study exercises, replacing all double quotes with singles doesn't work, because contraction has unescaped single quote

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,36 @@
+# defines the number of types of people as '10'
 types_of_people = 10
-x = "There are #{types_of_people} types of people."
-binary = "binary"
-do_not = "don't"
-y = "Those who know #{binary} and those who #{do_not}."
+# prints a string including the number of types of people as a variable
+x = 'There are #{types_of_people} types of people.'
+# defines the variable binary
+binary = 'binary'
+# defines the variable do_not as a contraction
+do_not = 'don't'
+# prints a string including variables binary and do_not
+y = 'Those who know #{binary} and those who #{do_not}.'
 
+# prints the string x
 puts x
+# prints the string y
 puts y
 
-puts "I said: #{x}."
-puts "I also said '#{y}'."
+# prints a line including the string x as a variable
+puts 'I said: #{x}.'
+# prints a line including the string y as a variable
+puts 'I also said '#{y}'.'
 
-hilarious = "false"
-joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# defines the variable hilarious as the string 'false'
+hilarious = 'false'
+# defines the variable joke_evaluation as a string containing the variable false
+joke_evaluation = 'Isn't that joke so funny?! #{hilarious}'
 
+# prints variable joke_evaluation
 puts joke_evaluation
 
-w = "This is the left side of..."
-e = "a string with a right side."
+# defines variable w as a string
+w = 'This is the left side of...'
+# defines the variable e as a string
+e = 'a string with a right side.'
 
+# prints the variables w and e together to output a single string
 puts w + e
