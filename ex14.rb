b906d66e943236d3816de2ebc480f1c04c45d3f2
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 20:34:56 -0800

    adds comments, changes prompt value, adds second ARGV value and fixes how it is asked for

--- ex14.rb
+++ ex14.rb
@@ -1,17 +1,29 @@
-user_name = ARGV.first
-prompt = '> '
+# sets first name as first ARGV and last name as second ARGV
+first, second = ARGV
+user_name = first + " " + second
+# defines prompt
+prompt = "prompt "
 
+# prints username
 puts "Hi #{user_name}."
+# prints string
 puts "I'd like to ask you a few questions."
+# asks question
 puts "Do you like me #{user_name}? ", prompt
+# sets answer as variable in a way I don't know
 likes = $stdin.gets.chomp
 
+# asks question
 puts "Where do you live #{user_name}? ", prompt
+# sets answer as user input variable in way I don't know
 lives = $stdin.gets.chomp
 
+# asks question
 puts "What kind of computer do you have? ", prompt
+# defines answer as variable of user input in method I don't know
 computer = $stdin.gets.chomp
 
+# prints multiline string with variables
 puts """
 Alright, so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
