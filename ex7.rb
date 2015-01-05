6a85e0aae2e5d9523aa0a705d31a19959f4a5e06
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 16:06:40 -0800

    adds comments to each line

--- ex7.rb
+++ ex7.rb
@@ -1,19 +1,35 @@
+# prints the line
 puts "Mary had a little lamb."
+# prints the line
 puts "Its fleece was white as #{'snow'}."
+# prints the line
 puts "And everywhere that Mary went."
+# prints the string 10 times
 puts "." * 10 # what'd that do? printed '.' 10 times
 
+# defines a variable
 end1 = "C"
+# defines a variable
 end2 = "h"
+# defines a variable
 end3 = "e"
+# defines a variable
 end4 = "e"
+# defines a variable
 end5 = "s"
+# defines a variable
 end6 = "e"
+# defines a variable
 end7 = "B"
+# defines a variable
 end8 = "u"
+# defines a variable
 end9 = "r"
+# defines a variable
 end10 = "g"
+# defines a variable
 end11 = "e"
+# defines a variable
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
@@ -21,5 +37,7 @@ end12 = "r"
 # Cheese
 # Burger
 # , as puts prints a line break
+# prints variables end1 - end5 and concatenates them, does not print a line break
 print end1 + end2 + end3 + end4 + end5 + end6
+# prints variables end6 - end12 and concatenates them, followed by a line break
 puts end7 + end8 + end9 + end10 + end11 + end12
