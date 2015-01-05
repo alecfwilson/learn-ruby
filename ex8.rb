9f17091750ae2e576410e1e425f77e7a86616488
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 16:36:51 -0800

    Zed's ex8

---
+++ ex8.rb
@@ -0,0 +1,15 @@
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you couldn't type right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
+# Exercise 8: Printing, Printing
