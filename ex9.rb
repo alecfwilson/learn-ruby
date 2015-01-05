be6b7c36c1525009c5de2db89bd36f6968f5cdd0
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 16:44:43 -0800

    adds comments and a question

--- ex9.rb
+++ ex9.rb
@@ -1,16 +1,21 @@
 # Here's some new strange stuff, remember type it exactly
 
+# defines variable days
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# defines variable months, \n is a line break in the string
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+# prints the string with the days variable
 puts "Here are the days: #{days}"
+# prints the string with the months variable
 puts "Here are the months: #{months}"
 
+# begins a multi-line string, cannot comment within it?
 puts """
 There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 """
-
+# ends multi-line string
 # Exercise 9 Printing, Printing, Printing
