756a5327c6bbf39d1689c22a98342d0ef3219b68
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 16:42:56 -0800

    Zed's ex9

---
+++ ex9.rb
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+# Exercise 9 Printing, Printing, Printing
