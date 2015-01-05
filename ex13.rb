37097e6c86e8904d8a3c08cf4a249509423fb202
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 19:55:13 -0800

    Zed's ex13

---
+++ ex13.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
+
+# Exercise 13: Parameters, Unpacking, Variables
