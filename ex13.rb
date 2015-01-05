95d330cdc4e884c56007ff164adf4efcbe67c3be
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 20:10:57 -0800

    adds comments and notes

--- ex13.rb
+++ ex13.rb
@@ -1,7 +1,11 @@
-first, second, third = ARGV
+# defines 3 variables as ARGV (argument variable)
+first, second, third = ARGV # not sure why Zed says 4 variables to the right are unpacked
 
+# prints first variable
 puts "Your first variable is #{first}"
+# prints second variable
 puts "Your second variable is #{second}"
+# prints third variable
 puts "Your third variable is #{third}"
 
 # Exercise 13: Parameters, Unpacking, Variables
