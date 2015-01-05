a341dd071c372da37e8c8038bcaeffdc6027bb3e
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-04 16:02:46 -0800

    answers comment questions

--- ex7.rb
+++ ex7.rb
@@ -1,7 +1,7 @@
 puts "Mary had a little lamb."
 puts "Its fleece was white as #{'snow'}."
 puts "And everywhere that Mary went."
-puts "." * 10 # what'd that do?
+puts "." * 10 # what'd that do? printed '.' 10 times
 
 end1 = "C"
 end2 = "h"
@@ -17,5 +17,9 @@ end11 = "e"
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
+# nothing visibly, but changing line 21 to puts would make the output:
+# Cheese
+# Burger
+# , as puts prints a line break
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
