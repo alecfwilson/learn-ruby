11c8eb02e0348faacc483ff6114e0a7526c5bbce
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-05 16:16:22 -0800

    Zed's ex17

---
+++ ex17.rb
@@ -0,0 +1,23 @@
+from_file, to_file = ARGV
+
+puts "Copying from #{from_file} tp #{to_file}"
+
+# we could do these two on one line, how?
+in_file = open(from_file)
+indata = in_file.read
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to about."
+$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done."
+
+out_file.close
+in_file.close
+
+# Exercise 17: More Files
