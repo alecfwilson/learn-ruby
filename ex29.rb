0508434eeaa037ba5762a31a4938a6a275a3e0d4
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-09 18:03:51 -0800

    adds comments, answers questions

--- ex29.rb
+++ ex29.rb
@@ -1,38 +1,71 @@
+# sets variable people to 20
 people = 20
+# sets variable cats to 30
 cats = 30
+# sets variable dogs to 15
 dogs = 15
 
 
+# evaluates boolean condition
 if people < cats
+  # if statement true, prints string
   puts "Too many cats! The world is doomed!"
+  # ends statement
 end
 
+# evaluates boolean condition
 if people > cats
+  # if condition true, prints string
   puts "Not many cats! The world is saved!"
+  # ends the statement
 end
 
+# evaluates condition
 if people < dogs
+  # if condition is true, print the string
   puts "The world is drooled on!"
+  # ends statement
 end
 
+# evaluates condition
 if people > dogs
+  # if true, prints the string
   puts "The world is dry!"
+  # ends statement
 end
 
 
+# increments variable dogs by 5
 dogs += 5
 
+# evaluates condition
 if people >= dogs
+  # if true, print the string
   puts "People are greater than or equal to dogs."
+  # ends statement
 end
 
+# evaluates condition
 if people <= dogs
+  # if true, print the string
   puts "People are less than or equal to dogs."
+  # ends statement
 end
 
 
+# evaluates condition
 if people == dogs
+  # if true, prints the string
   puts "People are dogs."
+  # ends statement
 end
+
+# The if statement asks Ruby to evaluate a condition
+  # with a result of a boolean. If true, execute code in statement
+  # if false, do not
+# the code under it is indented because it is the function to execute
+# if not indented, still works
+# Yes
+# The outputs change
 
 # Exercise 29: What If
