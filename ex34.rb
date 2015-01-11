38a4881c7d477bfb89f6b456662cde86b816d13f
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-11 15:38:52 -0800

    Zed's ex34

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is the second animal and is a #{animals[1]}. ruby"
+puts "The third (3rd) animal is at 2 and is a #{animals[2]}. peacock" 
+puts "The first (1st) animal is at 0 and is a #{animals[0]}. bear"
+puts "The animal at 3 is the fourth animal and is a #{animals[3]}. kangaroo"
+puts "The fifth (5th) animal is at 4 and is a #{animals[4]}. whale"
+puts "The animal at 2 is the third animal and is a #{animals[2]}. peacock"
+puts "The sixth (6th) animal is at 5 and is a #{animals[5]}. platypus"
+puts "The animal at 4 is the fifth animal and is a #{animals[4]}. whale"
+
+# Accessing elements of Arrays
---
+++ practice-programs/ruby
@@ -0,0 +1,12 @@
+aspirational_words = []
+word = gets.chomp
+
+word << aspirational_words.to_s
+
+puts aspirational_words.sample
+
+aspir_word = aspirational_words.sample
+
+puts "the word is #{aspir_word}"
+
+# aspirational words generator
