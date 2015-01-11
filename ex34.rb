82fefd8e2aad20d5b16a7a1c81212fb886cbf56f
Alec Wilson <alec.f.wilson@gmail.com>
2015-01-11 15:45:17 -0800

    adds line comments

--- ex34.rb
+++ ex34.rb
@@ -1,12 +1,21 @@
+# creates array animals
 animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
 
+# prints the 2nd animal at position 1
 puts "The animal at 1 is the second animal and is a #{animals[1]}. ruby"
+# prints the 3rd animal at position 2
 puts "The third (3rd) animal is at 2 and is a #{animals[2]}. peacock" 
+# prints the 1st animal at position 0
 puts "The first (1st) animal is at 0 and is a #{animals[0]}. bear"
+# prints the 4th animal at position 3
 puts "The animal at 3 is the fourth animal and is a #{animals[3]}. kangaroo"
+# prints the 5th animal at position 4
 puts "The fifth (5th) animal is at 4 and is a #{animals[4]}. whale"
+# prints the 3rd animal at position 2
 puts "The animal at 2 is the third animal and is a #{animals[2]}. peacock"
+# prints the 6th animal at position 5
 puts "The sixth (6th) animal is at 5 and is a #{animals[5]}. platypus"
+# prints the 5th animal at position 4
 puts "The animal at 4 is the fifth animal and is a #{animals[4]}. whale"
 
 # Accessing elements of Arrays
--- ex35.rb
+++ ex35.rb
@@ -1,3 +1,4 @@
-
+def gold_room
+  puts "This room is full of gold. How much do you take?"
 
 # Branches and Functions
